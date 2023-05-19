43cf977d754457cac9b2924e51f1b8f64779fb01
Weston Walker <walk8919@gmail.com>
2023-05-19 11:40:34 -0500

    Removed some unnecessary using directives.

--- BlazorSpark.Library/Mail/MailerTypes.cs
+++ BlazorSpark.Library/Mail/MailerTypes.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BlazorSpark.Library.Mail
+﻿namespace BlazorSpark.Library.Mail
 {
 	public class MailerTypes
 	{
