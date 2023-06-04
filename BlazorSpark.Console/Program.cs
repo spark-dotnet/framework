83a6ce3a10d04e8553744a7e6ca5f4e6b61bc11c
Weston Walker <walk8919@gmail.com>
2023-06-04 15:20:23 -0500

    Added CSS framework options to CLI and templates.

--- BlazorSpark.Console/Program.cs
+++ BlazorSpark.Console/Program.cs
@@ -7,8 +7,10 @@ using BlazorSpark.Console.Commands.Pages;
 using BlazorSpark.Console.Commands.Project;
 using BlazorSpark.Console.Commands.Services;
 using BlazorSpark.Console.Commands.Tasks;
+using BlazorSpark.Console.Shared;
 using McMaster.Extensions.CommandLineUtils;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Reflection;
 using System.Threading.Tasks;
@@ -19,12 +21,9 @@ namespace BlazorSpark.Console
     {
         public static void Main(string[] args)
         {
-            // TODO:
-            // spark open (start MyApp.sln)
-            // code generation
-            // code generation
             var app = new CommandLineApplication
             {
+                AllowArgumentSeparator = true,
                 Name = "spark"
             };
 
@@ -58,10 +57,11 @@ namespace BlazorSpark.Console
 
                 config.Description = "Create a new Blazor Spark project.";
                 var projectName = config.Argument<string>("name", "Name of the project to generate.");
+                var cssFramework = config.Option("-c|--css <CSS>", "CSS framework to use in Spark project", CommandOptionType.SingleValue);
                 config.OnExecute(() =>
                 {
                     string project = projectName.Value ?? null;
-                    new CreateProjectCommand().Execute(project);
+                    new CreateProjectCommand().Execute(project, cssFramework.Value());
                 });
             });
 
@@ -161,15 +161,6 @@ namespace BlazorSpark.Console
                         new CreateComponentCommand().Execute(componentName.Value);
                     });
                 });
-
-                // make migration DONE
-                // make event DONE
-                // make mail DONE
-                // make model DONE
-                // make service DONE
-                // make task DONE
-                // make page
-                // make component
             });
 
             app.Command("migrate", config =>
@@ -196,116 +187,6 @@ namespace BlazorSpark.Console
                 System.Console.WriteLine($"spark requires a command to do something");
                 return;
             }
-
-            // spark install
-            //if (command == "install")
-            //{
-            //	await InstallTemplate();
-            //	return;
-            //}
-
-
-            //// spark update
-            //if (command == "update")
-            //{
-            //    await Update();
-            //    return;
-            //}
-
-            //// spark new <ProjectName>
-            //if (command == "new")
-            //{
-            //	if (args.Length != 2)
-            //	{
-            //		Console.WriteLine($"spark new requires a project name. Ex: spark new <ProjectName>");
-            //		return;
-            //	}
-            //	var projectName = args[1];
-            //	await CreateProject(projectName);
-            //	return;
-            //}
-
-            //// spark make:migration <MigrationName>
-            //if (command == "make:migration")
-            //{
-            //	if (args.Length != 2)
-            //	{
-            //		Console.WriteLine($"spark make:migration requires a migration name. Ex: spark make:migration <MigrationName>");
-            //		return;
-            //	}
-            //	var name = args[1];
-            //	await CreateMigration(name);
-            //	return;
-            //}
-
-            //if (command == "run:migration")
-            //{
-            //	await RunMigration();
-            //	return;
-            //}
-
-            //async Task InstallTemplate()
-            //{
-            //	var psi = new ProcessStartInfo
-            //	{
-            //		FileName = "dotnet",
-            //		Arguments = "new install BlazorSpark.Templates"
-            //	};
-
-            //	using var proc = Process.Start(psi)!;
-            //	await proc.WaitForExitAsync();
-            //}
-
-            //async Task Update()
-            //{
-            //    Console.WriteLine($"Updating Spark Templates");
-            //    var psi2 = new ProcessStartInfo
-            //    {
-            //        FileName = "dotnet",
-            //        Arguments = "new install BlazorSpark.Templates"
-            //    };
-
-            //    using var proc2 = Process.Start(psi2)!;
-            //    await proc2.WaitForExitAsync();
-            //}
-
-            //async Task CreateProject(string projectName)
-            //{
-            //	Console.WriteLine($"Creating a Spark project at \"./{projectName}\"");
-            //	var psi = new ProcessStartInfo
-            //	{
-            //		FileName = "dotnet",
-            //		Arguments = $"new blazorspark -n {projectName} -o {projectName}"
-            //	};
-
-            //	using var proc = Process.Start(psi)!;
-            //	await proc.WaitForExitAsync();
-            //	Console.WriteLine($"Spark project {projectName} successfully created.");
-            //	Console.WriteLine($"Application ready! Build something amazing.");
-            //}
-
-            //async Task CreateMigration(string name)
-            //{
-            //	var psi = new ProcessStartInfo
-            //	{
-            //		FileName = "dotnet",
-            //		Arguments = $"ef migrations add {name} -o Application/Database/Migrations"
-            //	};
-
-            //    using var proc = Process.Start(psi)!;
-            //	await proc.WaitForExitAsync();
-            //}
-            //async Task RunMigration()
-            //{
-            //	var psi = new ProcessStartInfo
-            //	{
-            //		FileName = "dotnet",
-            //		Arguments = $"ef database update"
-            //	};
-
-            //	using var proc = Process.Start(psi)!;
-            //	await proc.WaitForExitAsync();
-            //}
         }
     }
 }
---
+++ BlazorSpark.Console/Shared/CssFrameworks.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSpark.Console.Shared
+{
+    public class CssFrameworks
+    {
+        public const string tailwind = nameof(tailwind);
+        public const string bootstrap = nameof(bootstrap);
+        public const string pico = nameof(pico);
+
+        public static bool IsValid(string framework)
+        {
+            if (framework == tailwind) return true;
+            if (framework == bootstrap) return true;
+            if (framework == pico) return true;
+            return false;
+        }
+    }
+}
