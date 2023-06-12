ef9e68e684101c2323223e284f4407878b1c93b6
Weston Walker <walk8919@gmail.com>
2023-06-12 14:52:37 -0500

    Added the first extension method, ToSlug. Added test for ToSlug.

---
+++ BlazorSpark.Library/Extensions/StringExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BlazorSpark.Library.Extensions
+{
+	public static class StringExtensions
+	{
+		public static string ToSlug(this string input)
+		{
+			string str = input.RemoveDiacritics().ToLower();
+			// invalid chars
+			str = Regex.Replace(str, @"[^a-z0-9\s-]", "");
+			// convert multiple spaces into one space
+			str = Regex.Replace(str, @"\s+", " ").Trim();
+			// cut and trim
+			str = str.Substring(0, str.Length <= 45 ? str.Length : 45).Trim();
+			// hyphens
+			str = Regex.Replace(str, @"\s", "-");
+			return str;
+		}
+
+		private static string RemoveDiacritics(this string str)
+		{
+			var normalizedString = str.Normalize(NormalizationForm.FormD);
+			var stringBuilder = new StringBuilder(capacity: normalizedString.Length);
+
+			for (int i = 0; i < normalizedString.Length; i++)
+			{
+				char c = normalizedString[i];
+				var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+				if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+				{
+					stringBuilder.Append(c);
+				}
+			}
+
+			return stringBuilder
+				.ToString()
+				.Normalize(NormalizationForm.FormC);
+		}
+	}
+}
---
+++ BlazorSpark.Tests/Extensions/ExtensionTests.cs
@@ -0,0 +1,23 @@
+﻿using BlazorSpark.Library.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSpark.Tests.Extensions
+{
+	[TestClass]
+	public class ExtensionTests
+	{
+		[TestMethod]
+		public void ShouldSlugifyString()
+		{
+			var str = "THis IS somE * String";
+
+			var slug = str.ToSlug();
+
+			Assert.AreEqual("this-is-some-string", slug);
+		}
+	}
+}
