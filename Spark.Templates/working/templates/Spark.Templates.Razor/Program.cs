6b8781787aadfa6214def052daf68d5698292b2b
Mario Andres Botero Florez <mariobot113@hotmail.com>
2023-07-18 19:49:22 -0500

    Add env, ignore, appsettings, program and readme to razor template

---
+++ Spark.Templates/working/templates/Spark.Templates.Razor/Program.cs
@@ -0,0 +1,32 @@
+using Spark.Library.Environment;
+using Spark.Library.Config;
+using Spark.Templates.Razor.Application.Startup;
+
+EnvManager.LoadConfig();
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.SetupSparkConfig();
+
+builder.Services.AddAppServices(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapRazorPages();
+
+app.Services.RegisterScheduledJobs();
+app.Services.RegisterEvents();
+
+app.Run();
