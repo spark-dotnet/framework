ae4e76ac8ceef65b59d1c0d2bcb75ff703be04cf
Weston Walker <walk8919@gmail.com>
2024-01-22 10:45:49 -0600

    Spark  migration command now tells you if your missing ef tools dependency. Fixed visual bug with navbar in spark template.

--- Spark.Console/Commands/Migrations/CreateMigrationCommand.cs
+++ Spark.Console/Commands/Migrations/CreateMigrationCommand.cs
@@ -2,10 +2,12 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Spark.Console.Commands.Migrations;
 
@@ -15,9 +17,36 @@ public class CreateMigrationCommand
     public void Execute(string migrationName)
     {
         ConsoleOutput.GenerateAlert(new List<string>() { $"Creating a new migration" });
-        Process.Start("dotnet", $"ef migrations add {migrationName} -o {MigrationPath}").WaitForExit();
-        ConsoleOutput.SuccessAlert(new List<string>() {
-            $"Spark migration {migrationName} created",
-        });
-    }
+        //Process.Start("dotnet", $"ef migrations add {migrationName} -o {MigrationPath}").WaitForExit();
+		ProcessStartInfo startInfo = new ProcessStartInfo();
+		startInfo.FileName = "dotnet"; 
+		startInfo.Arguments = $"ef migrations add {migrationName} -o {MigrationPath}";
+		startInfo.RedirectStandardOutput = true;
+		startInfo.RedirectStandardError = true;
+		startInfo.UseShellExecute = false;
+		startInfo.CreateNoWindow = true;
+		using (Process process = Process.Start(startInfo))
+		{
+			using (StreamReader outputReader = process.StandardOutput)
+			using (StreamReader errorReader = process.StandardError)
+			{
+				string output = outputReader.ReadToEnd();
+				string error = errorReader.ReadToEnd();
+
+				if (error.Contains("Could not execute because the specified command or file was not found"))
+				{
+					ConsoleOutput.ErrorAlert(new List<string>() { error });
+					ConsoleOutput.ErrorAlert(new List<string>() { "Miration was not created. Make sure  Entity Framework Core tools is installed." });
+					ConsoleOutput.ErrorAlert(new List<string>() { "You can install this by running - dotnet tool install --global dotnet-ef" });
+				}
+				else
+				{
+					ConsoleOutput.StandardAlert(new List<string>() { output });
+					ConsoleOutput.SuccessAlert(new List<string>() { $"Spark migration {migrationName} created" });
+				}
+			}
+
+			process.WaitForExit();
+		}
+	}
 }
